a2b46e8035d19b07b2ced863a970b18267af944e
zhang shuhao <shuhao_zhang@hust.edu.cn>
2025-10-25 00:47:39 +0800

    quick update

--- hello_world.py
+++ hello_world.py
@@ -13,9 +13,9 @@ Author: SAGE Team
 
 import time
 
-from sage.kernel.api.function.batch_function import BatchFunction
-from sage.kernel.api.function.map_function import MapFunction
-from sage.kernel.api.function.sink_function import SinkFunction
+from sage.common.core.functions.batch_function import BatchFunction
+from sage.common.core.functions.map_function import MapFunction
+from sage.common.core.functions.sink_function import SinkFunction
 from sage.kernel.api.local_environment import LocalEnvironment
 
 
